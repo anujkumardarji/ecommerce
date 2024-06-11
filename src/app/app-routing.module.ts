b790ecdfe50c976101be845e720168f5c0d6d3b0
Anujkumar Darji <anujdarji100737@gmail.com>
2024-06-12 00:09:33 +0400

    setup login, signup, product routes and basic design for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './no-auth/login/login.component';
+import { SignupComponent } from './no-auth/signup/signup.component';
+import { ProductsComponent } from './auth/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : LoginComponent,
+    pathMatch: 'full',
+  },
+  {
+    path : 'login',
+    component : LoginComponent
+  },
+  {
+    path : 'signup',
+    component : SignupComponent
+  },
+  {
+    path : 'products',
+    component : ProductsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './no-auth/login/login.component';
+import { SignupComponent } from './no-auth/signup/signup.component';
+import { ProductsComponent } from './auth/products/products.component';
+import { NavbarComponent } from './no-auth/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ProductsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
